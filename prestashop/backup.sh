920faabc830704977fc2c04cda46cd97ba126a99
ol1c <155087424+ol1c@users.noreply.github.com>
2025-11-24 19:27:31 +0100

    sql and img backup

--- prestashop/backup.sh
+++ prestashop/backup.sh
@@ -1,2 +1,2 @@
 docker exec prestashop-db mysqldump --default-character-set=utf8mb4 -u root -pprestashop prestashop > prestashop.sql
-docker exec prestashop-app tar czf - /var/www/html/img > img.tar.gz
+docker exec prestashop tar czf - /var/www/html/img > img.tar.gz
