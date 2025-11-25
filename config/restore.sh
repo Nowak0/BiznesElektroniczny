3e03d124acbc5116e7228bced432c26d7d963304
Nowak0 <kuubanowak@gmail.com>
2025-11-25 19:19:09 +0100

    hotfix

--- config/restore.sh
+++ config/restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat prestashop.sql | docker exec -i prestashop-db mysql --default-character-set=utf8mb4 -u root -pprestashop prestashop
-cat img.tar.gz | docker exec -i prestashop tar xzf - -C /var/www/html/img
-docker exec prestashop chown -R www-data:www-data /var/www/html/img/p
+cat img.tar.gz | docker exec -i prestashop tar xzf - -C /var/www/html
+docker exec prestashop chown -R www-data:www-data /var/www/html/img
 cat cod_module.tar.gz | docker exec -i prestashop tar xzf - -C /var/www/html/modules
 docker exec prestashop chown -R www-data:www-data /var/www/html/modules/ps_cashondelivery
