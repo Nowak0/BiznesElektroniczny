227ddd9b4329841c93221995d0d6babe680b7f4b
ol1c <155087424+ol1c@users.noreply.github.com>
2025-12-22 19:17:03 +0100

    win backup script

--- config/backup.ps1
+++ config/backup.ps1
@@ -1,9 +1,5 @@
 $ErrorActionPreference = "Stop"
 
-# Katalog na backup (na hoście)
-$backupDir = Join-Path (Get-Location) "backup"
-
-# Uruchom wszystko w CMD (ważne dla binarnego przekierowania >)
 cmd /c "if not exist backup mkdir backup"
 cmd /c "del /q backup\prestashop.sql backup\img.tar.gz backup\cod_module.tar.gz backup\slider_module.tar.gz 2>nul"
 
