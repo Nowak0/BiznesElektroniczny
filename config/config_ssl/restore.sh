3310976086d6dcdec28e89fb91d0049f5c0800ab
esce <p.demski100@gmail.com>
2026-01-06 18:42:58 +0100

    prod conf

--- config/config_ssl/restore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cat backup/prestashop.sql | docker exec -i prestashop-db mysql --default-character-set=utf8mb4 -u root -pprestashop prestashop
-cat backup/img.tar.gz | docker exec -i prestashop tar xzf - -C /var/www/html
-docker exec prestashop chown -R www-data:www-data /var/www/html/img
-cat backup/menu_module.tar.gz | docker exec -i prestashop tar xzf - -C /var/www/html/modules
-docker exec prestashop chown -R www-data:www-data /var/www/html/modules/ps_mainmenu
-cat backup/slider_module.tar.gz | docker exec -i prestashop tar xzf - -C /var/www/html/modules
-docker exec prestashop chown -R www-data:www-data /var/www/html/modules/ps_imageslider
